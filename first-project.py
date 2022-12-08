9f2f5e9616f1321dc0ea5c45e903f860c38ac4c6
Sulaev Tamerlan <sulaevtamerlan@gmail.com>
2022-12-08 14:28:25 +0300

    'initial'

---
+++ first-project.py
@@ -0,0 +1,10 @@
+number_1 = int(input('Введите число до которого нужно сделать таблицу: '))
+
+for table in range(1, number_1 + 1):
+    for n in range(1, 11):
+        answer = table * n
+        print(answer, end='\t')
+
+    print('\n')
+
+print('Две минуты. Готово!')
