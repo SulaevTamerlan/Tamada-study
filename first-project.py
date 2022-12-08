2ebc2d80401904de4725963edb78637305af5769
SulaevTamerlan <120032280+SulaevTamerlan@users.noreply.github.com>
2022-12-08 16:48:19 +0300

    Add files via upload

--- first-project.py
+++ first-project.py
@@ -1,10 +1,13 @@
 number_1 = int(input('Введите число до которого нужно сделать таблицу: '))
+if number_1 <= 10:
+    print('Считай сам!')
+else:
+    for table in range(1, number_1 + 1):
+        for n in range(1, 11):
+            answer = table * n
+            print(answer, end='\t')
 
-for table in range(1, number_1 + 1):
-    for n in range(1, 11):
-        answer = table * n
-        print(answer, end='\t')
+        print('\n')
 
-    print('\n')
+    print('Две минуты. Готово!')
 
-print('Две минуты. Готово!')
